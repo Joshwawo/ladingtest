43052f3318086f1feee0343ffee2e1e4f9a294ff
Joshwawo <masacota_masacota@hotmail.com>
2022-07-05 09:01:37 -0700

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {MenuAlt4Icon, XIcon} from '@heroicons/react/outline'
+
+const Navbar = () => {
+  return (
+    <div className='w-screen h=['>
+       <MenuAlt4Icon className='w-5'/>
+    </div>
+  )
+}
+
+export default Navbar
